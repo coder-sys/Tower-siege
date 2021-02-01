9bd9ab875334bb9c28d4a852eb2b1b79ce5c4bf0
coder-sys <unix99493@gmail.com>
2021-02-01 15:27:16 -0800

    This is updated

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ const World = Matter.World;
 const Engine = Matter.Engine;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-
+var s = 0
 var engine,world,bse,bs,scoreval,ma,sl,img;
 var boxes =[];
 
@@ -15,7 +15,7 @@ function setup(){
     world = engine.world;
     bse = new Obj(600,200,true);
     bs = new Obj(900,240,true);
-    ma = Bodies.rectangle(130,50,20,20)
+    ma = Bodies.rectangle(mouseX,mouseY,20,20)
     World.add(world,ma);
     sl = new Sling(ma)
     Engine.run(engine);
@@ -28,6 +28,9 @@ function keyPressed(){
     if(keyCode == 32){
     boxes.push(new Ho(mouseX,mouseY,20,20))
     }
+    if(keyCode == 65 && s1.sling.bodyA == null){
+        s1.sling.bodyA = ma
+    }
 }
 function draw(){
     background(0);
@@ -39,6 +42,7 @@ function draw(){
     translate(ma.position.x,ma.position.y);
     image(img,0,0,30,30)
     pop()
+    collision()
     for(var i = 0;i<boxes.length;i++){
         boxes[i].show()
     }
@@ -48,4 +52,18 @@ function draw(){
 }
 function mouseReleased(){
     sl.sling.bodyA = null
+}
+var a = 1
+collision = function(){
+for(var i =0;i<boxes.length;i++){
+    if(ma.position.x - boxes[i].body.position.x > 40){
+        s += a
+        console.log(s)
+        textSize(30)
+        text(s,700,400)
+        a = 0
+        break
+    }
+    break
+}
 }
